ff04c40328c1ed7df89695ed8256e82c1213c08d
wolfy1339 <webmaster@wolfy1339.com>
2023-05-13 22:09:38 -0400

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Octoherd script to replace Pika with ESBuild
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
